5373c99b1b432a49d98f389b7811896a67611812
victorverma <vishashv@gmail.com>
2024-11-17 23:58:17 -0500

    Fix indentation

--- scripts/download_raw_data.py
+++ scripts/download_raw_data.py
@@ -7,21 +7,21 @@ from datetime import datetime, timedelta
 from drms import Client
 
 def download_data(cli: Client, series: str, start_dttm: datetime, end_dttm: datetime, keywords: list[str], series_dir: str) -> None:
-  """
-  Download data on specified keywords for a given series over a given window of time.
+    """
+    Download data on specified keywords for a given series over a given window of time.
 
-  :param series: String that is the name of the series.
-  :param start_dttm: Datetime that is the beginning of the time window.
-  :param end_dttm: Datetime that is the end of the time window.
-  :param keywords: List of strings giving the keywords.
-  :param series_dir: String that is the path to the directory where the data should be saved.
-  """
-  ds = f"{series}[][{start_dttm.strftime('%Y.%m.%d_%H:%M:%S')}_TAI-{end_dttm.strftime('%Y.%m.%d_%H:%M:%S')}_TAI]"
-  key = ", ".join(keywords)
-  data = cli.query(ds, key=key, pkeys=True)
-  start_dt = start_dttm.strftime("%Y%m%d")
-  end_dt = end_dttm.strftime("%Y%m%d")
-  data.to_parquet(os.path.join(series_dir, f"{start_dt}-{end_dt}.parquet"))
+    :param series: String that is the name of the series.
+    :param start_dttm: Datetime that is the beginning of the time window.
+    :param end_dttm: Datetime that is the end of the time window.
+    :param keywords: List of strings giving the keywords.
+    :param series_dir: String that is the path to the directory where the data should be saved.
+    """
+    ds = f"{series}[][{start_dttm.strftime('%Y.%m.%d_%H:%M:%S')}_TAI-{end_dttm.strftime('%Y.%m.%d_%H:%M:%S')}_TAI]"
+    key = ", ".join(keywords)
+    data = cli.query(ds, key=key, pkeys=True)
+    start_dt = start_dttm.strftime("%Y%m%d")
+    end_dt = end_dttm.strftime("%Y%m%d")
+    data.to_parquet(os.path.join(series_dir, f"{start_dt}-{end_dt}.parquet"))
 
 parser = argparse.ArgumentParser(description="Download HARP data from JSOC")
 parser.add_argument("series", type=str, help="String; a SHARP parameter series")
@@ -44,10 +44,10 @@ cmd_args = parser.parse_args()
 cli = Client()
 sharp_series = cli.series(regex="hmi\\.sharp")
 if cmd_args.series not in sharp_series:
-  raise ValueError(f"{cmd_args.series} is not a SHARP parameter series. Valid series are {', '.join(sharp_series)}")
+    raise ValueError(f"{cmd_args.series} is not a SHARP parameter series. Valid series are {', '.join(sharp_series)}")
 series_dir = os.path.join("..", "data", "raw", cmd_args.series.replace('.', '_'))
 if not os.path.exists(series_dir):
-  raise ValueError(f"{series_dir} doesn't exist; make it before running this script")
+    raise ValueError(f"{series_dir} doesn't exist; make it before running this script")
 
 first_start_dttm = datetime.strptime(cmd_args.first_start_dt, "%Y%m%d")
 # The SHARP parameters are computed every 12 minutes, so 23:48:00 is the last
@@ -55,16 +55,16 @@ first_start_dttm = datetime.strptime(cmd_args.first_start_dt, "%Y%m%d")
 last_end_dttm = datetime.strptime(cmd_args.last_end_dt, "%Y%m%d").replace(hour=23, minute=48)
 start_dttms = pd.date_range(first_start_dttm, last_end_dttm, freq=cmd_args.step_size)
 if first_start_dttm not in start_dttms:
-  start_dttms = pd.DatetimeIndex([first_start_dttm]).append(start_dttms)
+    start_dttms = pd.DatetimeIndex([first_start_dttm]).append(start_dttms)
 end_dttms = start_dttms[1:] - timedelta(minutes=12)
 if last_end_dttm not in end_dttms:
-  end_dttms = end_dttms.append(pd.DatetimeIndex([last_end_dttm]))
+    end_dttms = end_dttms.append(pd.DatetimeIndex([last_end_dttm]))
 
 with open(cmd_args.keywords_file, "r") as file:
-  keywords = [line.strip() for line in file if line.strip()]
+    keywords = [line.strip() for line in file if line.strip()]
 
 # JSOC staff suggested submitting requests serially to avoid overloading the server; they also said that it
 # wouldn't be necessary to sleep between requests made in a serial fashion
 for i, (start_dttm, end_dttm) in enumerate(zip(start_dttms, end_dttms)):
-  download_data(cli, cmd_args.series, start_dttm, end_dttm, keywords, series_dir)
-  print(f"Downloaded data for {start_dttm}-{end_dttm}")
+    download_data(cli, cmd_args.series, start_dttm, end_dttm, keywords, series_dir)
+    print(f"Downloaded data for {start_dttm}-{end_dttm}")
