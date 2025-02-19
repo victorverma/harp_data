e91e51d0086fdcf38d0d4d4cab24dc6bb9466642
victorverma <vishashv@gmail.com>
2025-02-19 18:08:12 -0500

    Let user filter data during processing

--- scripts/process_raw_data.py
+++ scripts/process_raw_data.py
@@ -7,9 +7,15 @@ from drms import Client
 
 parser = argparse.ArgumentParser(description="Process raw HARP data downloaded from JSOC")
 parser.add_argument("--series", type=str, required=True, help="A data series for which raw data has been downloaded, e.g., hmi.sharp_cea_720s")
+parser.add_argument("--keep_low_qual_vals", action="store_true", help="Whether to keep predictor values in low-quality records (default: False)")
+parser.add_argument("--keep_near_limb_recs", action="store_true", help="Whether to keep records for near-limb HARPs (default: False)")
+parser.add_argument("--limb_threshold", type=float, default=70, help="Longitude threshold defining where the limb starts (default: 70)")
 cmd_args = parser.parse_args()
 series = cmd_args.series
 series_no_dot = series.replace(".", "_", 1)
+keep_low_qual_vals = cmd_args.keep_low_qual_vals
+keep_near_limb_recs = cmd_args.keep_near_limb_recs
+limb_threshold = cmd_args.limb_threshold
 
 print("Compiling the raw data", end="", flush=True)
 start_time = time.time()
@@ -48,6 +54,45 @@ data[number_cols] = data[number_cols].replace([np.inf, -np.inf], np.nan)
 elapsed_time = time.time() - start_time
 print(f"\rReplacing infinities with NaNs ({int(elapsed_time)}s)", flush=True)
 
+predictors = [
+    # SHARP parameters
+    "USFLUX", "MEANGAM", "MEANGBT", "MEANGBZ", "MEANGBH", "MEANJZD", "TOTUSJZ", "MEANALP",
+    "MEANJZH", "TOTUSJH", "ABSNJZH", "SAVNCPP", "MEANPOT", "TOTPOT", "MEANSHR", "SHRGT45",
+    # Patch areas and pixel counts
+    "NPIX", "SIZE", "AREA", "NACR", "SIZE_ACR", "AREA_ACR"
+]
+if not keep_low_qual_vals:
+    if (data.columns == "QUALITY").any():
+        print("Replacing low-quality values with NaNs", end="", flush=True)
+        start_time = time.time()
+        data.loc[data["QUALITY"] != 0, predictors] = np.nan
+        elapsed_time = time.time() - start_time
+        print(f"\rReplacing low-quality values with NaNs ({int(elapsed_time)}s)", flush=True)
+    else:
+        raise ValueError("To replace low-quality values with NaNs, QUALITY must be in the list of keywords")
+
+# Per (1), the level of noise in the SHARP parameters is substantially higher when the HARP is near the limb of the Sun. Per (3)-(5),
+# data measured on HARPs near the limb can be distorted by projection effects. Reference (1) seems to define the limb as the region
+# more than 45 degrees from the central meridian (see Section 9), while reference (2) uses a threshold of 70 degrees. However, in
+# (2), no X-class flare in the data occurred more than 68 degrees from the central meridian. References (3)-(5) use a threshold of 68
+# degrees, seemingly because of (2). Based on Table 4 in (1) and (5), the longitude of the flux-weighted center of the active pixels
+# (LON_FWT) is what needs to be within bounds.
+#
+# (1) https://doi.org/10.1007/s11207-014-0529-3
+# (2) https://dx.doi.org/10.1088/0004-637X/798/2/135
+# (3) https://doi.org/10.1029/2019SW002214
+# (4) https://doi.org/10.1029/2020SW002440
+# (5) https://doi.org/10.3847/1538-4357/ab89ac
+if not keep_near_limb_recs:
+    if (data.columns == "LON_FWT").any():
+        print("Discarding near-limb records", end="", flush=True)
+        start_time = time.time()
+        data = data[data["LON_FWT"].abs() <= limb_threshold]
+        elapsed_time = time.time() - start_time
+        print(f"\rDiscarding near-limb records ({int(elapsed_time)}s)", flush=True)
+    else:
+        raise ValueError("To discard near-limb records, LON_FWT must be in the list of keywords")
+
 def insert_missing_rows(group: pd.DataFrame) -> pd.DataFrame:
     """
     Insert rows in the Pandas DataFrame for a specified HARP for times that should have rows but do not.
@@ -74,7 +119,9 @@ print(f"\rInserting missing rows ({int(elapsed_time)}s)", flush=True)
 
 print("Saving the DataFrame", end="", flush=True)
 start_time = time.time()
-data.to_parquet(f"processed/{series_no_dot}.parquet")
+high_qual_suffix = "_high-qual" if not keep_low_qual_vals else ""
+near_center_suffix = f"_near-center-{limb_threshold}" if not keep_near_limb_recs else ""
+data.to_parquet(f"processed/processed{high_qual_suffix}{near_center_suffix}.parquet")
 elapsed_time = time.time() - start_time
 print(f"\rSaving the DataFrame ({int(elapsed_time)}s)", flush=True)
 print("Done")
